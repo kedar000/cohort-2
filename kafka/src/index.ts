0dd6fd1b154ebab748d1d95a36c0bd00ee80e06f
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-09-26 02:09:31 +0530

    added the kafka part

---
+++ kafka/src/index.ts
@@ -0,0 +1,43 @@
+// const { Kafka } = require('kafkajs')
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+
+  // change this broker to localhost
+  brokers: ['localhost:9092']
+})
+
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'quickstart-events',
+    messages: [
+      { value: 'Hello KafkaJS user!' },
+    ],
+  })
+
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'quickstart-events', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+        if(!message.value){
+            return;
+        }
+
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+
+run().catch(console.error)
