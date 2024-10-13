d80413add07d38ae1ab0f36d03887848d4840b8e
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-10-13 15:32:46 +0530

    created a trpc for adding a todo

--- trpc/src/index.ts
+++ trpc/src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello from ts ")
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
+
+const todoCreateType = z.object({
+    title : z.string(),
+    description : z.string()
+})
+const appRoter = router({
+    todoCreate : publicProcedure
+        .input(todoCreateType)
+        .mutation(async(opti) => {
+            const title = opti.input.title;
+            const description = opti.input.description
+            
+            // do db stuff here 
+            return {
+                id : 1,
+                title : title,
+                description :description
+            };
+        })
+})
+
+export type appRoter = typeof appRoter;  
